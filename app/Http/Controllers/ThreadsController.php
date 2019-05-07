a5bae0d1b2adeb63a8839458ff81219d8a772677
ahmedmzaki <ahmedmzaki22@gmail.com>
2019-05-07 14:11:41 +0200

    removing project comments

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -15,11 +15,7 @@ class ThreadsController extends Controller
          $this->middleware('auth')->except(['index','show']);
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index($channelSlug = null)
     {
         if ($channelSlug)
@@ -34,23 +30,14 @@ class ThreadsController extends Controller
     }
 
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create()
     {
         $channels = Channel::all();
         return view('threads.create',compact('channels'));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
         $request->validate([
@@ -70,36 +57,19 @@ class ThreadsController extends Controller
          return redirect('/threads/'.$channelName.'/'.$thread->id);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Thread  $thread
-     * @return \Illuminate\Http\Response
-     */
+
     public function show($channelId,Thread $thread)
     {
        return view('threads.show',compact('thread'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Thread  $thread
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Thread $thread)
     {
         $channels = Channel::all();
         return view('threads.edit',compact('channels','thread'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Thread  $thread
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, Thread $thread)
     {
         $request->validate([
@@ -116,12 +86,7 @@ class ThreadsController extends Controller
         return redirect('/threads/'.$channelName.'/'.$thread->id);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Thread  $thread
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy(Thread $thread)
     {
         $thread->delete();
